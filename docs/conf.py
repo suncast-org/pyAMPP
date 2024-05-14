91b9f84bbf708c100e5bcbf288da5fc9717067d8
Sijie Yu <sjyu1988@gmail.com>
2024-05-13 22:32:13 -0400

    Fix ReadTheDocs build issues

--- docs/conf.py
+++ docs/conf.py
@@ -20,17 +20,18 @@ author = "suncast-org"
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.inheritance_diagram",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.doctest",
-    "sphinx.ext.mathjax",
-    "sphinx_automodapi.automodapi",
-    "sphinx_automodapi.smart_resolver",
+    'sphinx.ext.autodoc',  # Automatically document your code
+    'sphinx.ext.napoleon',  # Support for Google-style docstrings
+    'sphinx.ext.viewcode',  # Add links to source code from documentation
+    'sphinx.ext.mathjax',  # Render math via JavaScript
+    'sphinx.ext.autosummary',  # Automatically generates summary tables from the docstrings.
+    'sphinx.ext.githubpages',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.imgmath',
+    # 'sphinx_gallery.gen_gallery',
+    # 'sphinx_gallery',
+    'autoapi.extension'
+    # Add any other Sphinx extensions here.
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,7 +40,12 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -59,16 +65,29 @@ intersphinx_mapping = {"python": ("https://docs.python.org/", None)}
 # a list of builtin themes.
 html_theme = "alabaster"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
 
-# By default, when rendering docstrings for classes, sphinx.ext.autodoc will
-# make docs with the class-level docstring and the class-method docstrings,
-# but not the __init__ docstring, which often contains the parameters to
-# class constructors across the scientific Python ecosystem. The option below
-# will append the __init__ docstring to the class-level docstring when rendering
-# the docs. For more options, see:
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
-autoclass_content = "both"
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using a default sidebar with
+# the links to the documentation's roots, contents & search, plus a link to
+# the Python.org website.
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+        'donate.html',
+    ]
+}
+
+# -- Options for sphinx.ext.autodoc -------------------------------------------
+autoapi_type = 'python'
+autoapi_dirs = ['../pyampp']
+autoapi_ignore = [
+    'tests/*',
+    '_dev/*',
+    'lib/*'
+]
